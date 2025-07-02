3a052d868e4476b5a7467048d9837e3a6a655a1c
zakyyl <zaky24112003@gmail.com>
2025-07-02 11:05:26 +0700

    update index dashboard.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,39 +4,51 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Carbon;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        
-        $totalRawatJalan = Cache::remember('total_rawat_jalan', 300, function () {
+        $bulanIni = Carbon::now()->format('Y-m');
+
+        $totalRawatJalan = Cache::remember("total_rawat_jalan_$bulanIni", 10, function () use ($bulanIni) {
             return DB::table('reg_periksa')
                 ->where('kd_pj', 'BP1')
                 ->where('status_lanjut', 'ralan')
+                ->where('tgl_registrasi', 'like', "$bulanIni%")
                 ->count();
         });
 
-        $totalRawatInap = Cache::remember('total_rawat_inap', 300, function () {
+        $totalRawatInap = Cache::remember("total_rawat_inap_$bulanIni", 10, function () use ($bulanIni) {
             return DB::table('reg_periksa')
                 ->where('kd_pj', 'BP1')
                 ->where('status_lanjut', 'ranap')
+                ->where('tgl_registrasi', 'like', "$bulanIni%")
                 ->count();
         });
 
-        $totalPasien = Cache::remember('total_pasien', 300, function () {
-            return DB::table('pasien')->count();
+        $pengajuanRawatJalan = Cache::remember("pengajuan_ralan_$bulanIni", 10, function () use ($bulanIni) {
+            return DB::table('mlite_vedika')
+                ->where('jenis', 'ralan')
+                ->where('status', 'Pengajuan')
+                ->where('tanggal', 'like', "$bulanIni%")
+                ->count();
         });
 
-        $totalDokter = Cache::remember('total_dokter', 300, function () {
-            return DB::table('dokter')->count();
+        $pengajuanRawatInap = Cache::remember("pengajuan_ranap_$bulanIni", 10, function () use ($bulanIni) {
+            return DB::table('mlite_vedika')
+                ->where('jenis', 'ranap')
+                ->where('status', 'Pengajuan')
+                ->where('tanggal', 'like', "$bulanIni%")
+                ->count();
         });
 
         return view('dashboard.index', compact(
             'totalRawatJalan',
             'totalRawatInap',
-            'totalPasien',
-            'totalDokter'
+            'pengajuanRawatJalan',
+            'pengajuanRawatInap'
         ));
     }
 }
