ac64da48c178977da31584e32a9225582231926a
zakyyl <zaky24112003@gmail.com>
2025-07-02 14:53:11 +0700

    update interface dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,59 +4,47 @@
 <div class="container-xxl flex-grow-1 container-p-y">
   <h4 class="fw-bold mb-4">Dashboard</h4>
 
-  <div class="row g-3">
-    <div class="col-md-6 col-xl-3">
-      <div class="card shadow-sm border-0">
-        <div class="card-body d-flex align-items-center">
-          <div class="avatar bg-primary text-white me-3 rounded">
-            <i class="bx bx-walk fs-4"></i>
-          </div>
-          <div>
-            <h6 class="mb-1">Rawat Jalan</h6>
-            <h4 class="mb-0">{{ $totalRawatJalan }}</h4>
-          </div>
+  <div class="row">
+    <div class="col-md-3">
+        <div class="info-box bg-info">
+            <span class="info-box-icon"><i class="fas fa-procedures"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Rawat Jalan Bulan Ini</span>
+                <span class="info-box-number">{{ $totalRawatJalan }}</span>
+            </div>
         </div>
-      </div>
     </div>
-    <div class="col-md-6 col-xl-3">
-      <div class="card shadow-sm border-0">
-        <div class="card-body d-flex align-items-center">
-          <div class="avatar bg-success text-white me-3 rounded">
-            <i class="bx bx-hotel fs-4"></i>
-          </div>
-          <div>
-            <h6 class="mb-1">Rawat Inap</h6>
-            <h4 class="mb-0">{{ $totalRawatInap }}</h4>
-          </div>
+    
+    <div class="col-md-3">
+        <div class="info-box bg-warning">
+            <span class="info-box-icon"><i class="fas fa-clipboard-list"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Pengajuan Ralan</span>
+                <span class="info-box-number">{{ $pengajuanRawatJalan }}</span>
+            </div>
         </div>
-      </div>
     </div>
-    <div class="col-md-6 col-xl-3">
-      <div class="card shadow-sm border-0">
-        <div class="card-body d-flex align-items-center">
-          <div class="avatar bg-info text-white me-3 rounded">
-            <i class="bx bx-user fs-4"></i>
-          </div>
-          <div>
-            <h6 class="mb-1">Jumlah Pasien</h6>
-            <h4 class="mb-0">{{ $totalPasien }}</h4>
-          </div>
+    <div class="col-md-3">
+        <div class="info-box bg-primary">
+            <span class="info-box-icon"><i class="fas fa-procedures"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Rawat Inap Bulan Ini</span>
+                <span class="info-box-number">{{ $totalRawatInap }}</span>
+            </div>
         </div>
-      </div>
     </div>
-    <div class="col-md-6 col-xl-3">
-      <div class="card shadow-sm border-0">
-        <div class="card-body d-flex align-items-center">
-          <div class="avatar bg-warning text-white me-3 rounded">
-            <i class="bx bx-user-pin fs-4"></i>
-          </div>
-          <div>
-            <h6 class="mb-1">Jumlah Dokter</h6>
-            <h4 class="mb-0">{{ $totalDokter }}</h4>
-          </div>
+
+
+    <div class="col-md-3">
+        <div class="info-box bg-danger">
+            <span class="info-box-icon"><i class="fas fa-clipboard-check"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Pengajuan Ranap</span>
+                <span class="info-box-number">{{ $pengajuanRawatInap }}</span>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
+
 </div>
 @endsection
