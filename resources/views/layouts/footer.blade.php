05298cc72fc47f692177374946d5616e54e78c0e
zakyyl <zaky24112003@gmail.com>
2025-07-04 12:04:33 +0700

    add version info.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,7 @@
 <footer class="main-footer">
-  <div class="float-right d-none d-sm-inline">
-    VEDIKA
+  <div class="float-right d-none d-sm-inline text-sm text-muted">
+    <i class="fas fa-code-branch"></i> VEDIKA v1.0 &mdash; Dirilis perdana Juli 2025 &bull; 
+    <i class="far fa-clock"></i> Terakhir diperbarui {{ date('d F Y') }}
   </div>
-  <strong>&copy; {{ date('Y') }}</strong>
+  <strong class="text-sm">&copy; {{ date('Y') }} VEDIKA</strong>
 </footer>
