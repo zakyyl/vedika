ca124356c2ff2d5f8cb57157ff9506f27c974beb
zakyyl <zaky24112003@gmail.com>
2025-08-11 11:56:35 +0700

    fixing show field data.

--- resources/views/rawatinap/partials/labpa.blade.php
+++ resources/views/rawatinap/partials/labpa.blade.php
@@ -13,35 +13,67 @@
         <div id="collapseLabPA" class="collapse" aria-labelledby="headingLabPA" data-parent="#accordionLabPA">
             <div class="card-body p-2">
                 @if (!empty($laboratorium_pa) && count($laboratorium_pa) > 0)
-                    <div class="table-responsive">
-                        <table class="table table-sm table-bordered mb-0">
-                            <thead class="bg-light">
-                                <tr>
-                                    <th>Tanggal</th>
-                                    <th>Jam</th>
-                                    <th>Dokter</th>
-                                    <th>Pemeriksaan</th>
-                                    <th>Diagnosa Klinik</th>
-                                    <th>Makroskopik</th>
-                                    <th>Mikroskopik</th>
-                                    <th>Kesan</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($laboratorium_pa as $row)
-                                    <tr>
-                                        <td>{{ $row->tgl_periksa ?? '-' }}</td>
-                                        <td>{{ $row->jam ?? '-' }}</td>
-                                        <td>{{ $row->dokter_perujuk ?? '-' }}</td>
-                                        <td>{{ $row->pemeriksaan ?? '-' }}</td>
-                                        <td>{{ $row->diag_klinik ?? '-' }}</td>
-                                        <td>{{ $row->makroskopik ?? '-' }}</td>
-                                        <td>{{ $row->mikroskopik ?? '-' }}</td>
-                                        <td>{{ $row->kesan ?? '-' }}</td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                    <div class="accordion" id="hasilLabPAAccordion">
+                        @foreach ($laboratorium_pa as $index => $pa)
+                            @php
+                                $collapseId = 'collapseLabPA_' . $index;
+                                $headingId = 'headingLabPA_' . $index;
+                            @endphp
+                            <div class="card mb-2">
+                                <div class="card-header py-2 px-3" id="{{ $headingId }}">
+                                    <h6 class="mb-0">
+                                        <button class="btn btn-link btn-sm w-100 text-left collapsed" type="button"
+                                            data-toggle="collapse" data-target="#{{ $collapseId }}"
+                                            aria-expanded="false" aria-controls="{{ $collapseId }}">
+                                            <strong>{{ $pa->tgl_periksa ? \Carbon\Carbon::parse($pa->tgl_periksa)->format('d M Y') : '-' }}</strong>
+                                            - {{ $pa->jam ?? '-' }}
+                                            <span class="float-right text-muted">
+                                                <i class="fas fa-vial"></i> No Lab: {{ $pa->nolab ?? '-' }}
+                                            </span>
+                                        </button>
+                                    </h6>
+                                </div>
+                                <div id="{{ $collapseId }}" class="collapse"
+                                    aria-labelledby="{{ $headingId }}" data-parent="#hasilLabPAAccordion">
+                                    <div class="card-body p-2">
+                                        <div class="table-responsive">
+                                            <table class="table table-sm table-bordered mb-0">
+                                                <tbody>
+                                                    <tr>
+                                                        <th width="20%">Cara</th>
+                                                        <td>{{ $pa->cara ?? '-' }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Lokasi</th>
+                                                        <td>{{ $pa->lokasi ?? '-' }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Diagnosa Klinik</th>
+                                                        <td>{{ $pa->diagnosa_klinik ?? '-' }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Makroskopik</th>
+                                                        <td>{{ $pa->makroskopik ?? '-' }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Mikroskopik</th>
+                                                        <td>{{ $pa->mikroskopik ?? '-' }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Kesan</th>
+                                                        <td>{{ $pa->kesan ?? '-' }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Kesimpulan</th>
+                                                        <td>{{ $pa->kesimpulan ?? '-' }}</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
                 @else
                     <div class="alert alert-info m-0">
