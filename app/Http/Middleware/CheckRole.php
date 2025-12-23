2f12da75e78ad79ec2574923dd9a5506d9d4497a
zakyyl <zaki24112003@gmail.com>
2025-12-23 10:52:45 +0700

    create new middleware.

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$roles): Response
+    {
+        if (!auth()->check()) {
+            abort(403, 'Unauthorized action.');
+        }
+
+        $userRole = auth()->user()->roles;
+
+        if (!in_array($userRole, $roles)) {
+            abort(403, 'Anda tidak memiliki akses ke halaman ini.');
+        }
+
+        return $next($request);
+    }
+}
